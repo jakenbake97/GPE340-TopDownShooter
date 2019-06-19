ff4ee202ff31a36a33396f064b240785776796d6
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-18 21:05:29 -0500

    Format for Discussion and Fixed over-rotation bug
    -Added header and summary tags
    -Fixed over-rotation by setting the y value of target point to character transform Y value

--- Top Down Shooter/Assets/Scripts/CharacterAnimationController.cs
+++ Top Down Shooter/Assets/Scripts/CharacterAnimationController.cs
@@ -2,14 +2,14 @@
 
 public class CharacterAnimationController : MonoBehaviour
 {
-    [SerializeField, Tooltip("The max speed of the player (in meters per second")]
+    [Header("Movement Settings")] [SerializeField, Tooltip("The max speed of the player (in meters per second")]
     private float moveSpeed = 4f;
 
     [SerializeField, Tooltip("The rotations speed of the player (in degrees per second)")]
     private float rotationSpeed = 90f;
 
     private Animator anim;
-    
+
     // int variables that hold a converted animator look up to increase performance, per the suggestion of Rider
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
     private static readonly int Vertical = Animator.StringToHash("Vertical");
@@ -20,29 +20,29 @@ public class CharacterAnimationController : MonoBehaviour
     }
 
 
-    public void SetSpeed(Vector3 inputVector) //takes input from input manager, converts it to world space,
-                                              //gives it a magnitude equal to the max move speed and then passes
-                                              //the input to the setAnimationProperties method
+    ///<summary>takes input from input manager, converts it to local space, gives it a magnitude equal to the
+    /// max move speed and then passes the input to the setAnimationProperties method </summary>
+    public void SetSpeed(Vector3 inputVector)
     {
         inputVector = transform.InverseTransformDirection(inputVector);
         inputVector *= moveSpeed;
         SetAnimationProperties(inputVector);
     }
 
-    private void SetAnimationProperties(Vector3 inputVector) //sets the float parameters of the animator using the
-                                                             //input vector and a hash ID representing the
-                                                             //variable string name
+    ///<summary>sets the float parameters of the animator using the input vector and a hash ID representing the
+    ///variable string name</summary>
+    private void SetAnimationProperties(Vector3 inputVector)
     {
         anim.SetFloat(Horizontal, inputVector.x);
         anim.SetFloat(Vertical, inputVector.z);
     }
 
-    public void RotateTowardsPoint(Vector3 targetPoint) //Takes a target point from the mouse position of the
-                                                        //input manager and creates a quaternion for the rotation
-                                                        //needed between the target point and current position,
-                                                        //then rotates the game object towards the target by
-                                                        //rotationSpeed degrees per second
+    ///<summary>Takes a target point from the mouse position of the input manager and creates a quaternion for
+    ///the rotation needed between the target point and current position, then rotates the game object towards
+    /// the target by rotationSpeed degrees per second </summary>
+    public void RotateTowardsPoint(Vector3 targetPoint)
     {
+        targetPoint = new Vector3(targetPoint.x, transform.position.y, targetPoint.z);
         Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
         transform.rotation =
             Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
