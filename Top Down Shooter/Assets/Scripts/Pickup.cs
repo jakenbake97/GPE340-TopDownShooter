67dfa20bee0961c1600f5daa25385374f77f07ae
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-20 18:31:09 -0500

    Create Pickup Class

---
+++ Top Down Shooter/Assets/Scripts/Pickup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private float lifeSpan = 60f;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Decay(lifeSpan);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        RotatePickup(30f);
+        
+    }
+
+    private void Decay(float lifeSpan)
+    {
+        Destroy(gameObject, lifeSpan);
+    }
+
+    private void RotatePickup(float rotationAmount)
+    {
+        transform.Rotate(Vector3.up, rotationAmount * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        throw new NotImplementedException();
+    }
+}
