7bd20d5057e501a2f6e3cf11c88accefdb529b63
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-21 11:04:58 -0500

    Add Damage and Heal pickups

---
+++ Top Down Shooter/Assets/Scripts/DamagePickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePickup : Pickup
+{
+    [SerializeField, Tooltip("The amount of damage to apply")]
+    private float damage = 10f;
+
+    protected override void OnPickUp(InputManager player)
+    {
+        var playerHealth = player.gameObject.GetComponent<Health>();
+        playerHealth.Damage(damage);
+        base.OnPickUp(player);
+    }
+}
---
+++ Top Down Shooter/Assets/Scripts/HealPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPickup : Pickup
+{
+    [SerializeField, Tooltip("The amount to heal")]
+    private float heal = 10f;
+
+    protected override void OnPickUp(InputManager player)
+    {
+        var playerHealth = player.gameObject.GetComponent<Health>();
+        playerHealth.Heal(heal);
+        base.OnPickUp(player);
+    }
+}
