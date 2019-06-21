4b5dfb301478b4a1adc5d3e20508ab852b4a15ff
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-21 10:27:33 -0500

    Update Health.cs
    -Added functionality and events to health.cs
    -Attached health component to player
    -Renamed player

--- Top Down Shooter/Assets/Scripts/Health.cs
+++ Top Down Shooter/Assets/Scripts/Health.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
@@ -10,28 +12,48 @@ public class Health : MonoBehaviour
     //ability to heal
     //notify other objects of state changes when taking damage or when destroyed
     // Start is called before the first frame update
-    
-    [SerializeField, Tooltip("The maximum health allowed")]
+    [Header("Health Stats"), SerializeField, Tooltip("The maximum health allowed")]
     private float maxHealth = 200f;
 
     [SerializeField, Tooltip("The starting health value")]
     private float initialHealth = 200f;
 
-    private float healthValue;
+    public float HealthValue { get; private set; }
 
-//    public float HealthValue
-//    {
-//        get;
-//        private set { healthValue = value; }
-//    }
-//
-//    public float HealthPercent
-//    {
-//        get;
-//        private set
-//        {
-//            
-//        }
-//    }
-  
+    public float HealthPercent => HealthValue / maxHealth;
+
+    [Header("Events"), SerializeField, Tooltip("Activated every time the object is healed")]
+    private UnityEvent onHeal;
+
+    [SerializeField, Tooltip("Activated every time the object is damaged")]
+    private UnityEvent onDamage;
+
+    [SerializeField, Tooltip("Activated once when the object's healthValue reaches 0")]
+    private UnityEvent onDie;
+
+    public void Damage(float damage)
+    {
+        if (damage < 0) //damage must be positive
+        {
+            damage *= -1;
+        }
+
+        HealthValue -= damage;
+        onDamage.Invoke();
+        if (HealthValue <= 0f)
+        {
+            onDie.Invoke();
+        }
+    }
+
+    public void Heal(float heal)
+    {
+        if (heal < 0) //heal must be positive
+        {
+            heal *= -1;
+        }
+
+        HealthValue = Mathf.Clamp(HealthValue + heal, 0f, maxHealth);
+        onHeal.Invoke();
+    }
 }
