049c967a1b10f71fc528fe516ab4a40548fc70e8
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-28 13:41:29 -0500

    Weapon Agent Foundations

--- Top Down Shooter/Assets/Scripts/Enemy.cs
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : MonoBehaviour
     private Animator anim;
 
 
-    [SerializeField] private Transform target;
+    private Player target;
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
     private static readonly int Vertical = Animator.StringToHash("Vertical");
     private Vector3 desiredVelocity;
@@ -27,7 +27,16 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        agent.SetDestination(target.position);
+        target = FindObjectOfType<Player>();
+        if (!target)
+        {
+            agent.isStopped = true;
+            anim.SetFloat(Horizontal, 0f);
+            anim.SetFloat(Vertical, 0f);
+            return;
+        }
+
+        agent.SetDestination(target.transform.position);
         desiredVelocity = agent.desiredVelocity;
         Vector3 input = transform.InverseTransformDirection(desiredVelocity);
         anim.SetFloat(Horizontal, input.x);
---
+++ Top Down Shooter/Assets/Scripts/WeaponAgent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Animator), typeof(CapsuleCollider))]
+[RequireComponent(typeof(Health))]
+public class WeaponAgent : MonoBehaviour
+{
+    private Health health;
+
+    private Animator anim;
+
+    [SerializeField] protected GameObject equippedWeapon;
+
+    private Weapon currentWeapon;
+
+    [SerializeField] protected Transform attachmentPoint;
+
+    public virtual void Awake()
+    {
+        health = GetComponent<Health>();
+        anim = GetComponent<Animator>();
+        currentWeapon = equippedWeapon.GetComponent<Weapon>();
+    }
+}
