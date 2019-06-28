6499ec3f70a78377d3ddcdb41937591821e10bb0
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-27 22:57:27 -0500

    Root Motion NavMesh & Off-Mesh links lectures

--- Top Down Shooter/Assets/Scripts/Enemy.cs
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -1,25 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
 public class Enemy : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator anim;
 
 
-    [SerializeField] Transform target;
+    [SerializeField] private Transform target;
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private Vector3 desiredVelocity;
+
 
     // Start is called before the first frame update
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         agent.SetDestination(target.position);
+        desiredVelocity = agent.desiredVelocity;
+        Vector3 input = transform.InverseTransformDirection(desiredVelocity);
+        anim.SetFloat(Horizontal, input.x);
+        anim.SetFloat(Vertical, input.z);
+    }
+
+    private void OnAnimatorMove()
+    {
+        agent.velocity = anim.velocity;
     }
 }
