98fbd94a14af73d860d56d7567be6b9adfa62fa5
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-06-27 19:25:36 -0500

    Navmesh & NavmeshAgent tutorial

---
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+
+    [SerializeField] Transform target;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
