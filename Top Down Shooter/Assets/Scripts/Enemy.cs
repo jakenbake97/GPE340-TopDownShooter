dc60ff2bfc9871519bccc19cf6a2707efc25f718
jakenbake97 <31628783+jakenbake97@users.noreply.github.com>
2019-07-07 22:06:24 -0500

    Fix agent not moving to new target
    -Change agent.isStopped to false when enemy has a target

--- Top Down Shooter/Assets/Scripts/Enemy.cs
+++ Top Down Shooter/Assets/Scripts/Enemy.cs
@@ -51,6 +51,7 @@ public class Enemy : WeaponAgent
             return false;
         }
 
+        agent.isStopped = false;
         agent.SetDestination(GameManager.Player.transform.position);
         desiredVelocity = agent.desiredVelocity;
         Vector3 input = transform.InverseTransformDirection(desiredVelocity);
